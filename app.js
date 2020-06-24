140dc9a325e8f35bbf996979951d058561ffe8ab
Wiretys <wiretys@gmail.com>
2020-06-23 23:58:16 -0400

    added source js file

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
+// console.log(profileDataArgs);
+
+// const printProfileData = profileDataArr => {
+//   for (let i = 0; i < profileDataArr.length; i += 1) {
+//     console.log(profileDataArr[i]);
+//   }
+//   console.log('================');
+
+//   profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+// printProfileData(profileDataArgs);
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
 
-const printProfileData = profileDataArr => {
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
-  console.log('================');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+const name = profileDataArgs[0];
+const github = profileDataArgs[1];
 
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-printProfileData(profileDataArgs);
+
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
